7eeeef8ea21d3f3b8d664a020b03e231a15839d3
Alex Chalupka <lupka@few.io>
2022-03-17 13:04:18 -0500

    hide edit functionality

--- resources/js/components/StripeDetails.vue
+++ resources/js/components/StripeDetails.vue
@@ -1,9 +1,9 @@
 <template>
-  <loading-view 
-    :loading="loading" 
+  <loading-view
+    :loading="loading"
     class="w-full">
-    <div 
-      v-if="!subscription" 
+    <div
+      v-if="!subscription"
       class="flex py-8">
       <button
         class="mx-auto btn btn-default bg-30 border-30 font-normal hover:bg-40"
@@ -18,31 +18,31 @@
         Stripe subscription management
       </heading>
 
-      <display-row 
-        v-if="subscription" 
+      <display-row
+        v-if="subscription"
         label="Created">
         {{ subscription.created_at }}
       </display-row>
 
-      <display-row 
-        v-if="subscription" 
+      <display-row
+        v-if="subscription"
         label="Plan">
         {{ subscription.stripe_plan }}
       </display-row>
 
-      <display-row 
-        v-if="subscription" 
+      <!-- <display-row
+        v-if="subscription"
         label="Change plan">
-        <select 
-          v-model="newPlan" 
+        <select
+          v-model="newPlan"
           class="form-control form-select">
-          <option 
-            value="" 
-            disabled="disabled" 
+          <option
+            value=""
+            disabled="disabled"
             selected="selected">Choose New Plan</option>
-          <option 
-            v-for="plan in plans" 
-            :key="plan.id" 
+          <option
+            v-for="plan in plans"
+            :key="plan.id"
             :value="plan.id">
             {{ plan.id }} ({{ plan.price / 100 }} {{ plan.currency }} / {{ plan.interval }})
           </option>
@@ -60,27 +60,27 @@
         >
           Update Plan
         </button>
-      </display-row>
+      </display-row> -->
 
-      <display-row 
-        v-if="subscription" 
+      <display-row
+        v-if="subscription"
         label="Amount">
         {{ subscription.plan_amount / 100 }} ({{ subscription.plan_currency }}) /
         {{ subscription.plan_interval }}
       </display-row>
 
-      <display-row 
-        v-if="subscription" 
+      <display-row
+        v-if="subscription"
         label="Billing period">
         {{ subscription.current_period_start }} => {{ subscription.current_period_end }}
       </display-row>
 
-      <display-row 
-        v-if="subscription" 
+      <display-row
+        v-if="subscription"
         label="Status">
         <span v-if="subscription.on_grace_period">On Grace Period</span>
-        <span 
-          v-if="subscription.cancelled || subscription.cancel_at_period_end" 
+        <span
+          v-if="subscription.cancelled || subscription.cancel_at_period_end"
           class="text-danger"
         >Cancelled</span
         >
@@ -91,7 +91,7 @@
         >Active</span
         >
 
-        <button
+        <!-- <button
           v-if="
             subscription.active && !subscription.cancelled && !subscription.cancel_at_period_end
           "
@@ -99,7 +99,7 @@
           @click="$emit('cancel-subscription')"
         >
           Cancel
-        </button>
+        </button> -->
 
         <button
           v-if="subscription.active && subscription.cancel_at_period_end"
@@ -111,8 +111,8 @@
       </display-row>
     </div>
 
-    <invoices-table 
-      v-if="invoices.length" 
+    <invoices-table
+      v-if="invoices.length"
       :invoices="invoices" />
   </loading-view>
 </template>
